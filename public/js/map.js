2d500a565bf362837e7786d61446ec9e577c4f30
Vishawdeep-Singh <vishawdeepsingh29@gmail.com>
2024-06-09 15:35:48 +0530

    Added some responsiveness

--- public/js/map.js
+++ public/js/map.js
@@ -4,7 +4,7 @@ console.log(maptoken);
 mapboxgl.accessToken = maptoken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/satellite-streets-v12',
+   
     center: coordinates, // starting position [lng, lat]
     zoom: 13 // starting zoom
 });
@@ -13,8 +13,16 @@ const map = new mapboxgl.Map({
 const marker = new mapboxgl.Marker({color: "red"})
 .setLngLat(coordinates)
 .addTo(map);
-const popup = new mapboxgl.Popup({offset: 25})
-    .setLngLat(coordinates)
-    .setHTML(`<h1>${title1}</h1>`)
-    .setMaxWidth("300px")
-    .addTo(map);
+
+
+
+
+
+    const element = document.querySelector('.mapper');
+    const screenWidth = window.innerWidth;
+    console.log(screenWidth)
+    if (screenWidth < 1024) {
+        element.classList.remove('offset-3');
+      } else{
+        element.classList.add('offset-3');
+      }
